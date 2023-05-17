d2dc98bebb3d7e3aa03f27de01f8c0a72485895b
Shwarz1Van1Hummer <113684065+Shwarz1Van1Hummer@users.noreply.github.com>
2023-05-17 14:15:19 +0600

    #ver2

--- src/main/java/com/example/bank/Models/Account.java
+++ src/main/java/com/example/bank/Models/Account.java
@@ -1,4 +1,25 @@
 package com.example.bank.Models;
 
-public class Account {
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public abstract class Account {
+    private final StringProperty owner;
+    private final StringProperty accountNumber;
+    private final DoubleProperty balance;
+
+    public Account(String Owner, String accountNumber, double balance) {
+        this.owner = new SimpleStringProperty(this, "Owner", Owner);
+        this.accountNumber = new SimpleStringProperty(this, "Account number", accountNumber);
+        this.balance = new SimpleDoubleProperty(this, "Balance", balance);
+
+    }
+
+    public StringProperty ownerProperty() {return owner;}
+
+    public StringProperty accountNumberProperty() {return accountNumber;}
+
+    public DoubleProperty balanceProperty() {return balance;}
 }
